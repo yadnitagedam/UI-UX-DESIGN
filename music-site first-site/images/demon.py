b156e0960207f46de29c4e07df52ccb17e36b7ab
yadnitagedam <149067592+yadnitagedam@users.noreply.github.com>
2024-02-11 14:15:24 +0530

    Add files via upload

---
+++ music-site first-site/images/demon.py
@@ -0,0 +1,29 @@
+# import pyttsx3
+
+
+# engine = pyttsx3.init('sapi5')
+# voices = engine.getproperty('voices')
+# # print(voice[1].id)
+# engine.setproperty('voice', voices[0].id)
+
+
+# def speak(audio):
+#     engine.say(audio)
+#     engine.runAndWait()
+
+
+# def wishme():
+#     hour = int (datetime.datetime.now().hour)
+#     if hour>=0 and hour<12:
+#         speak("Good Morning!")
+
+# elif hour>=12 and hour<18:
+#         speak("Good Afternoon!")
+
+#     else:
+#         speak("Good Evening!")
+
+#     speak("I am jarvis mam. please tell me how many I help you")
+
+# if_name_== "_main_":
+#   speak("i love you mahi")
